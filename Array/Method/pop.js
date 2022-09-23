c9016945f3aaa79047230a6392afb72e81d5e831
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-23 22:50:11 +0300

    Created Pop

---
+++ Array/Method/pop.js
@@ -0,0 +1,15 @@
+//Pop====================================================================================================
+
+const myArray = [1, 2, 3]
+console.log(myArray)
+
+myArray.pop()
+
+console.log(myArray)
+
+const removedElement = myArray.pop()
+
+console.log(myArray)
+
+console.log(removedElement)
+//====================================================================================================
