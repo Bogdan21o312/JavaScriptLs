5d6d6fe4eec9d8156eea92861592582190174c80
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-24 17:15:55 +0300

    ArrayCycles ForEach

---
+++ Array/Cycles/forEach.js
@@ -0,0 +1,13 @@
+//ArrayForEach====================================================================================================
+
+const myArray = [
+    'First', 'Second', 'Third',
+    'First', 'Second', 'Third',
+    'First', 'Second', 'Third',
+]
+
+myArray.forEach((element, index) => {
+    console.log(element, index)
+})
+
+//====================================================================================================
